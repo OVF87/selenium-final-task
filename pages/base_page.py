2d3f95b321640420aef5a5a257ff8c76a4fa6a27
OVF <okarskiy@gmail.com>
2023-03-03 13:23:15 +0400

    add init, conftest, tetst_main_page, requirements, pages

---
+++ pages/base_page.py
@@ -0,0 +1,11 @@
+# Напишите здесь свой код :-)
+class BasePage():
+
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+
+
+    def open(self):
+        self.browser.get(self.url)
---
+++ test_main_page.py
@@ -0,0 +1,10 @@
+# Напишите здесь свой код :-)
+from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
