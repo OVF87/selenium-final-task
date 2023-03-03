6eaef524fc68e0640338e870133d64354051eb01
OVF <okarskiy@gmail.com>
2023-03-03 14:50:29 +0400

    add locators, login page

---
+++ locators.py
@@ -0,0 +1,18 @@
+# Напишите здесь свой код :-)
+
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+    #LOGIN_EMAIL_LINK = (By.CSS_SELECTOR, '#login_form input[type="email"]')
+    #LOGIN_PASSWORD_LINK = (By.CS_SELECTOR, '#login_form input[type="passwors"]')
+    #REGISTER_EMAIL_LINK = (By.CSS_SELECTOR, '#register_form input[type="email"]')
+    #REGISTER_PASSWORD_LINK = (By.CSS_SELECTOR, '#register_form input[name*="password1"]')
+    #REGISTER_CONFIRM_PASSWORD_LINK = (By.CSS_SELECTOR, '#register_form input[name*="password2"]')
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 # Напишите здесь свой код :-)
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -9,8 +10,19 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
+
