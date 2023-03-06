35e8246a507c7fbd3ec0e44df41685e3e4145efc
OVF <okarskiy@gmail.com>
2023-03-06 13:45:43 +0400

    add new test

--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ import pytest
 from pages.product_page import ProductPage
 from selenium.webdriver.common.by import By
 
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -15,7 +16,44 @@ from selenium.webdriver.common.by import By
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
     #проверка страницы продукта
-    #link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_product_in_basket()
+    page.should_be_click_add_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_product_in_basket()
+    page.should_be_price_product_equal_total()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    #Проверяем, что нет сообщения об успехе not_element_present
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209'
+    page = ProductPage(browser, link, 0)
+    page.open()
+    page.should_be_basket_url()
+    page.should_be_click_add_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    #Проверяем, что нет сообщения об успехе с помощью not_element_present
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209'
+    page = ProductPage(browser, link, 0)
+    page.open()
+    page.should_be_basket_url()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    #Проверяем, что нет сообщения об успехе с помощью is_disappeared
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209'
+    page = ProductPage(browser, link, 0)
+    page.open()
+    page.should_be_basket_url()
+    page.should_be_click_add_basket()
+    page.should_not_be_disappeared_success_message()
+
+
+
+
