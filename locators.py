2d07193e43c759c664716ceafe3d2dd1b3e23798
OVF <okarskiy@gmail.com>
2023-03-07 12:50:30 +0400

    refactoring

--- locators.py
+++ locators.py
@@ -1,10 +1,26 @@
-# Напишите здесь свой код :-)
+# Определение локаторов
 from selenium.webdriver.common.by import By
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, "a[href*='basket']")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
+class BasketPageLocators():
+    BASKET_TEXT_IS_EMPTY = (By.CSS_SELECTOR, '#content_inner p')
+    BASKET_IS_EMPTY = (By.CSS_SELECTOR, '#content_inner h2')
 
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "input[name*='registration-email']")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "input[name*='password1']")
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "input[name*='password2']")
+    BTN_REGISTER = (By.CSS_SELECTOR, "button[name*='regist']")
 
 
 class ProductPageLocators():
@@ -14,13 +30,3 @@ class ProductPageLocators():
     PRICE_PRODUCT = (By.CSS_SELECTOR, "p[class*='price']")
     NAME_PRODUCT = (By.CSS_SELECTOR, 'div h1')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, 'div.alertinner ')
-
-
-class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    BASKET_LINK = (By.CSS_SELECTOR, "a[href*='basket']")
-
-class BasketPageLocators():
-    BASKET_TEXT_IS_EMPTY = (By.CSS_SELECTOR, '#content_inner p')
-    BASKET_IS_EMPTY = (By.CSS_SELECTOR, '#content_inner h2')
